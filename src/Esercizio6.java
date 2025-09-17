a78b817c77b1eda1b2b17b7609496ca6206d03e1
lauraoliveri <lauraoliveri2003@gmail.com>
2025-09-17 16:18:43 +0200

    Esercizio 6

---
+++ src/Esercizio6.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Esercizio6 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero");
+
+        int n = scan.nextInt();
+
+        for ( int i = 0; i < n; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("@");
+            }
+            System.out.println("@");
+        }
+
+    }
+}
+
+
+/* Scrivi un programma Java che, dato un numero in input, stampi un triangolo come nell'esempio.
+
+Esempio:
+
+Inserisci il numero: 6
+Uscita prevista:
+      @
+     @@
+    @@@
+   @@@@
+  @@@@@
+ @@@@@@
+
+ */
