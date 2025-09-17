ace72b0ba9d9f11c097b5674cd4d3241901d1474
lauraoliveri <lauraoliveri2003@gmail.com>
2025-09-17 12:11:54 +0200

    Esercizio 2

---
+++ src/Esercizio2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Esercizio2 {
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        System.out.print("I primi 10 numeri naturali sono:");
+        for (int i = 1; i <= numero ; i++) {
+
+            System.out.print(i + " ");
+
+        }
+    }
+}
